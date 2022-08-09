93394298c4e69d0e8b50f784a87f8173a5d45b04
santoyox714 <santoyox714@gmail.com>
2022-08-09 10:26:35 -0700

    update admin.py view

--- playground_root/pages/models.py
+++ playground_root/pages/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Page(models.Model):
-    title = models.CharField(max_length=60)
-    permalink = models.CharField(max_length=12, unique=True)
-    update_date = models.DateTimeField('Last Updated')
-    bodytext = models.TextField('Page Content', blank = True)
+class Page(models.Model):               
+    title = models.CharField(max_length=60) #page title to be used in html
+    permalink = models.CharField(max_length=12, unique=True)   #permalink 
+    update_date = models.DateTimeField('Last Updated')          #update date
+    bodytext = models.TextField('Page Content', blank = True)   #bodytext
+    
+    def __str__(self):
+            return self.title
+
