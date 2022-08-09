9921eb0ae62a5b27bd2f0f0f34ed3768aa164508
santoyox714 <santoyox714@gmail.com>
2022-08-09 11:25:18 -0700

    Update admin.py

--- playground_root/pages/admin.py
+++ playground_root/pages/admin.py
@@ -4,3 +4,11 @@ from .models import Page #imports page model to admin view
 
 # Register your models here.
 admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','update_date')
+    ordering=('title',)
+    search_field=('title',)
+
+admin.site.register(Page, PageAdmin)
+
